d67cce5be2d74d60547f70ade8300d744aaad7ba
Zhalgas Azhibayev <g.zhiga@gmail.com>
2023-02-09 22:35:27 +0600

    konva

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-stage :config="configKonva" class="stage">
+        <v-layer>
+        </v-layer>
+    </v-stage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      configKonva: {
+        width: 200,
+        height: 200
+      },
+    };
   }
-}
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
